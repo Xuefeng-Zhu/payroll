742efdb2fca7d92ccea343e4075023ea5daedacb
Xuefeng Zhu <xzhu15@illinois.edu>
2017-08-22 01:14:21 -0500

    Update payroll.sol

--- payroll.sol
+++ payroll.sol
@@ -1,51 +1,49 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.14;
 
 contract Payroll {
     uint constant payDuration = 10 seconds;
 
     address owner;
-    address employee;
     uint salary;
-    uint lastPaidDay;
+    address employee;
+    uint lastPayday;
 
     function Payroll() {
         owner = msg.sender;
     }
-
-    function updateEmployee(address newEmployee, uint newSalary) {
+    
+    function updateEmployee(address e, uint s) {
         require(msg.sender == owner);
         
         if (employee != 0x0) {
-            uint payment = salary * (now - lastPaidDay) / payDuration;
-            assert(payment <= this.balance);
+            uint payment = salary * (now - lastPayday) / payDuration;
             employee.transfer(payment);
         }
-
-        employee = newEmployee;
-        salary = newSalary * 1 ether;
-        lastPaidDay = now;
+        
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
     }
-
+    
     function addFund() payable returns (uint) {
-        require(msg.value > 0);
         return this.balance;
     }
-
-    function calculateRunWay() returns (uint) {
-        assert(salary > 0);
+    
+    function calculateRunway() returns (uint) {
         return this.balance / salary;
     }
-
+    
     function hasEnoughFund() returns (bool) {
-        return calculateRunWay() > 0;
+        return calculateRunway() > 0;
     }
-
+    
     function getPaid() {
-        require(employee == msg.sender);
+        require(msg.sender == employee);
         
-        uint nextPayDay = lastPaidDay + payDuration;
-        assert(now > nextPayDay);
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
         employee.transfer(salary);
-        lastPaidDay = nextPayDay;
     }
 }
