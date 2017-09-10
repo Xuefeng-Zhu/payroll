2220cb326e0edb7f4c76a1eb2068a2eedfc24f4d
Xuefeng Zhu <xzhu15@illinois.edu>
2017-09-10 16:47:55 -0500

    Update payroll.sol

--- payroll.sol
+++ payroll.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.14;
 
-contract Payroll {
+import './SafeMath.sol';
+import './Ownable.sol';
+
+contract Payroll is Ownable {
     using SafeMath for uint;
     struct Employee {
         address id;
@@ -10,18 +13,8 @@ contract Payroll {
     
     uint constant payDuration = 10 seconds;
 
-    address owner;
     uint totalSalary;
     mapping(address => Employee) public employees;
-
-    function Payroll() {
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
     
     modifier employeeExit(address employeeId) {
         var employee = employees[employeeId];
@@ -30,7 +23,9 @@ contract Payroll {
     }
     
     function _partialPaid(Employee employee) private {
-        uint payment = employee.salary * (now.sub(employee.lastPayday)) / payDuration;
+        uint payment = employee.salary
+            .mul(now.sub(employee.lastPayday))
+            .div(payDuration);
         employee.id.transfer(payment);
     }
 
@@ -38,15 +33,15 @@ contract Payroll {
         var employee = employees[employeeId];
         assert(employee.id == 0x0);
 
-        employees[employeeId] = Employee(employeeId, salary * 1 ether, now);
-        totalSalary += employees[employeeId].salary;
+        employees[employeeId] = Employee(employeeId, salary.mul(1 ether), now);
+        totalSalary = totalSalary.add(employees[employeeId].salary);
     }
     
     function removeEmployee(address employeeId) onlyOwner employeeExit(employeeId) {
         var employee = employees[employeeId];
 
         _partialPaid(employee);
-        totalSalary -= employee.salary;
+        totalSalary = totalSalary.sub(employee.salary);
         delete employees[employeeId];
     }
     
@@ -54,10 +49,10 @@ contract Payroll {
         var employee = employees[employeeId];
 
         _partialPaid(employee);
-        totalSalary -= employee.salary;
-        employee.salary = salary * 1 ether;
+        totalSalary = totalSalary.sub(employee.salary);
+        employee.salary = salary.mul(1 ether);
         employee.lastPayday = now;
-        totalSalary += employee.salary;
+        totalSalary = totalSalary.add(employee.salary);
     }
     
     function addFund() payable returns (uint) {
@@ -65,7 +60,7 @@ contract Payroll {
     }
     
     function calculateRunway() returns (uint) {
-        return this.balance / totalSalary;
+        return this.balance.div(totalSalary);
     }
     
     function hasEnoughFund() returns (bool) {
@@ -75,7 +70,7 @@ contract Payroll {
     function getPaid() employeeExit(msg.sender) {
         var employee = employees[msg.sender];
 
-        uint nextPayday = employee.lastPayday + payDuration;
+        uint nextPayday = employee.lastPayday.add(payDuration);
         assert(nextPayday < now);
 
         employee.lastPayday = nextPayday;
